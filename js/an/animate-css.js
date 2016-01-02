d35e4fd1bcc40478934fe563b78d2644cdaa946a
vasiliy28 <vasiliy13022015@gmail.com>
2016-01-02 09:23:42 +0300

    new

---
+++ js/an/animate-css.js
@@ -0,0 +1,71 @@
+//Animate CSS + WayPoints javaScript Plugin
+//Example: $(".element").animated("zoomInUp", "zoomOutDown");
+//Author URL: http://webdesign-master.ru
+(function($) {
+	'use strict'
+		$.fn.animated = function(inEffect, outEffect,offsetBottom,offsetTop) {
+			var bottomDef = "85%";
+			var topDef = "15%";
+
+			var bottom;
+			var top;
+
+			if (typeof offsetBottom == "undefined") {
+				bottom = bottomDef;
+
+			} else {
+				if (typeof(offsetBottom) == "string") {
+					if (offsetBottom.match(/%/i) == null) {
+						bottom = bottomDef;
+
+					} else {
+						bottom = offsetBottom;
+
+					}
+
+				}
+				else {
+					bottom = $(window).height() - offsetBottom;
+				}
+			}
+			if (typeof offsetTop == "undefined") {
+				top = topDef;
+
+			} else {
+				if (typeof(offsetTop) == "string") {
+					if (offsetTop.match(/%/i) == null) {
+						top = topDef;
+
+					} else {
+						top = offsetTop;
+
+					}
+
+				}
+				else {
+					top = offsetTop;
+				}
+			}
+
+
+			$(this).css("opacity", "0").addClass("animated pointerEvents").waypoint(function (dir) {
+				if (dir === "down") {
+					$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+				} else {
+					$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+				}
+				;
+			}, {
+				offset: bottom
+			}).waypoint(function (dir) {
+				if (dir === "down") {
+					$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+				} else {
+					$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+				}
+				;
+			}, {
+				offset: top
+			});
+		};
+})(jQuery);
--- js/myJS.js
+++ js/myJS.js
@@ -1,24 +1,51 @@
-/**
- * Created by vasy on 22.12.15.
- */
-var backImg=$('#back img');
-function chengHeight(){
-    var backImgHeigth=backImg.css('height');
-    var headerPage = $('#headerPage');
-    headerPage.css('height',backImgHeigth);
-};
 
-$(window).ready(chengHeight());
-$(window).on('resize',chengHeight);
+    $(window).ready(function(){
+        'use strict'
 
-$(window).scroll(function(){
-    var st =$(this).scrollTop();
-    console.log(st);
 
-    $('#back').css({
-        "transform":"translate(0% , " + st/15 + "%"    })
+        function chengHeight() {
+            var backImg = $('#back img');
+            backImg.css("max-height",$(window).height())
+            var backImgHeigth = backImg.css('height');
+            var headerPage = $('#headerPage');
+            headerPage.css('height', backImgHeigth);
+        };
 
-})
-$("#togNav").click(function(){
-    $("#navPage").toggleClass('active')
-})
+        chengHeight();
+        $(window).on('resize',chengHeight);
+
+        $(window).scroll(function(){
+            var st =$(this).scrollTop();
+
+            $('#back').css({
+                "transform":"translate(0% , " + st/15 + "%"    })
+
+        })
+        $("#togNav").click(function(){
+            $("#navPage").toggleClass('active')
+        })
+        centre();
+
+        $('#allWorks').mixItUp({
+            callbacks: {
+                onMixEnd: function(){
+
+                    animateCss()
+
+                }
+            }
+        });
+
+        function centre(){
+            var b =  $(".mainLinkWrap");
+            var w = b.width();
+            console.log(w/2)
+            b.css("margin-left",-(w/2))
+        }
+        function animateCss(){
+            $(".ourWork header h1").animated("slideInRight","slideOutRight",50,0)
+            $(".ourWork header h3").animated("slideInRight","slideOutRight",50,0)
+            $(".ourWork header div").animated("slideInRight","slideOutRight",50,30)
+        }
+
+    })
